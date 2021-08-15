5df2fb51df919bab7546f9005fb6a3b477bac1a1
Azahar2 <a.zahar@baro-pfannenstein.de>
2021-08-15 20:06:04 +0200

    FETCH AND UPDATE POSTS FUNCTIONALITY

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import "./App.css";
+const apiUrl = "http://localhost:3004/posts";
 function App() {
+  const [postList, setPostList] = useState([]);
+  const [newPostTitle, setPostTitle] = useState("");
+
+  async function fetchPostList() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    setPostList(data);
+  }
+
+  async function createPost(postData) {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    fetchPostList();
+    // setPostList(data);
+  }
+
+  useEffect(() => {
+    fetchPostList();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {postList.map((post) => {
+        return <div>{post.title}</div>;
+      })}
+      <input
+        value={newPostTitle}
+        onChange={(e) => setPostTitle(e.target.value)}
+      ></input>
+      <button
+        onClick={() => {
+          createPost({ title: newPostTitle, author: "user" });
+          setPostTitle("");
+        }}
+      >
+        Create a fake post
+      </button>
     </div>
   );
 }
